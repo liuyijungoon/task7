135b812abbf8439b46f27ed5a5b949796138f6e9
liuyijungoon <1019205777@qq.com>
2017-08-10 05:35:43 +0000

    task7

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -25,6 +25,13 @@ describe("测试描述", function(){
         main();
         var result = _.flatten(console.log.args).join("\n");
         var expect_string = '';
+    for(var i=99;i>1;i--)
+    {
+        expect_string=expect_string+i+' bottles of beer on the wall,'+i+' bottles of beer.\n'+'Take one down and pass it around,'+(i-1)+' bottles of beer on the wall.\n';
+        //console.log(i+' bottles of beer on the wall,'+i+' bottles of beer.\n'+'Take one down and pass it around,'+(i-1)+' bottles of beer on the wall.')
+    }
+    expect_string=expect_string+1+' bottles of beer on the wall,'+1+' bottles of beer.\n'+'Take one down and pass it around,'+'no more bottles of beer on the wall.\n'+'No more bottles of beer on the wall,no more bottles of beer.\n'+'Go to the store and buy some more,99 bottles of beer on the wall.'
+    return expect_string;
 
         expect(expect_string).to.equal(result);
     });
